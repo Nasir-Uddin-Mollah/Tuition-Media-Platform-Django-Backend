5230ea96fb0ef753eedaece257a96c03eaf9c778
Nasir <nasirhossenmollah@gmail.com>
2025-04-25 19:55:50 +0600

    added queryset

--- tuition/views.py
+++ tuition/views.py
@@ -16,3 +16,11 @@ class TuitionViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.TuitionSerializer
     filter_backends = [filters.SearchFilter]
     search_fields = ['class_name__name']
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        id = self.request.query_params.get('id')
+
+        if id:
+            queryset = queryset.filter(id=id)
+        return queryset
