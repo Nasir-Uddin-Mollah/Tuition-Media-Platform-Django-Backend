a23208f597e525bd5cb1b5519de685cef323e1c2
Nasir <nasirhossenmollah@gmail.com>
2025-04-29 16:08:21 +0600

    added StringRelatedField

--- application/serializers.py
+++ application/serializers.py
@@ -3,12 +3,15 @@ from . import models
 
 
 class ApplicationSerializer(serializers.ModelSerializer):
+    tuition = serializers.StringRelatedField(many=False)
     class Meta:
         model = models.Application
         fields = '__all__'
 
 
 class ReviewSerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField(many=False)
+    tuition = serializers.StringRelatedField(many=False)
     class Meta:
         model = models.Review
         fields = '__all__'
